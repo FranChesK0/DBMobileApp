0bc32b822e29265434b1394a94158bef4faa70b8
FranChesKo <khl_doss@mail.ru>
2023-12-07 07:15:06 +0800

    Create select_all

--- src/database/queries/__init__.py
+++ src/database/queries/__init__.py
@@ -1,6 +1,7 @@
-from .queries import create_tables, insert
+from .queries import create_tables, insert, select_all
 
 __all__ = [
     "create_tables",
-    "insert"
+    "insert",
+    "select_all"
 ]
--- src/database/queries/queries.py
+++ src/database/queries/queries.py
@@ -1,5 +1,10 @@
+from sqlalchemy import select
+
 from database import engine, session_factory
 from database.models import BaseModel
+from misc import LoggerName, get_logger
+
+logger = get_logger(LoggerName.DATABASE)
 
 
 def create_tables() -> None:
@@ -7,10 +12,16 @@ def create_tables() -> None:
     BaseModel.metadata.create_all(engine)
 
 
-def insert(orm: BaseModel | list[BaseModel]):
+def insert(orm: BaseModel | list[BaseModel]) -> None:
     with session_factory() as session:
         if isinstance(orm, BaseModel):
             session.add(orm)
         elif isinstance(orm, list):
             session.add_all(orm)
         session.commit()
+
+
+def select_all(orm: type[BaseModel]) -> list[BaseModel]:
+    with session_factory() as session:
+        query = select(orm)
+        return list(session.execute(query).scalars().all())
