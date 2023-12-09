edb40c453f89cd85df04ccd081c4b79ba307ecec
FranChesKo <khl_doss@mail.ru>
2023-12-09 13:15:21 +0800

    Add fill_talbes function

--- src/database/queries/__init__.py
+++ src/database/queries/__init__.py
@@ -1,7 +1,9 @@
 from .queries import create_tables, insert, select_all
+from .fill_tables import fill_tables
 
 __all__ = [
     "create_tables",
     "insert",
-    "select_all"
+    "select_all",
+    "fill_tables"
 ]
