1df917707a9bdb587e14b013ac8b4acf4df64d33
FranChesKo <khl_doss@mail.ru>
2023-12-17 14:01:19 +0800

    Add tables to queries

--- src/database/queries/__init__.py
+++ src/database/queries/__init__.py
@@ -1,12 +1,13 @@
 from .queries import (create_tables,
                       insert,
                       select_all,
-                      select_by_parameter)
+                      select_by_column)
 from .fill_tables import fill_tables
 
 __all__ = [
     "create_tables",
     "insert",
     "select_all",
+    "select_by_column",
     "fill_tables"
 ]
--- src/database/queries/queries.py
+++ src/database/queries/queries.py
@@ -1,9 +1,8 @@
 from sqlalchemy import select
-from sqlalchemy.orm import joinedload, selectinload, contains_eager
 
 from database import engine, session_factory
-from database import models
 from database.database_types import PkTypes
+from database.tables import TableType, BaseTable
 from database.models import BaseModel, BaseModelType
 from misc import LoggerName, get_logger
 
@@ -13,7 +12,9 @@ logger = get_logger(LoggerName.DATABASE)
 async def create_tables() -> None:
     async with engine.begin() as connection:
         await connection.run_sync(BaseModel.metadata.drop_all)
+        logger.warning("Database was dropped")
         await connection.run_sync(BaseModel.metadata.create_all)
+        logger.warning("Database was created")
 
 
 async def insert(orm: BaseModelType | list[BaseModelType]) -> None:
@@ -25,37 +26,20 @@ async def insert(orm: BaseModelType | list[BaseModelType]) -> None:
         await session.commit()
 
 
-async def select_all(orm: type[BaseModelType]) -> list[BaseModelType]:
+async def select_all(table: type[TableType], relationships: bool = False) -> list[BaseTable.DTO | BaseTable.REL_DTO]:
+    dto = table.REL_DTO if relationships else table.DTO
     async with session_factory() as session:
-        query = select(orm).options(*_get_options(orm))
-        return list((await session.execute(query)).unique().scalars().all())
+        query = select(table.ORM).options(*table.OPTIONS) if relationships else select(table.ORM)
+        return [dto.model_validate(row, from_attributes=True)
+                for row in (await session.execute(query)).unique().scalars().all()]
 
 
-async def select_by_parameter(orm: type[BaseModelType], name: str, value: any) -> list[BaseModelType]:
+async def select_by_column(table: type[TableType], column: str, value: any,
+                           relationships: bool = False) -> list[BaseTable.DTO | BaseTable.REL_DTO]:
+    dto = table.REL_DTO if relationships else table.DTO
     async with session_factory() as session:
-        query = select(orm).options(*_get_options(orm)).filter_by(**{name: value})
-        return list((await session.execute(query)).unique().scalars().all())
-
-
-def _get_options(orm: type[BaseModelType]):
-    match orm:
-        case models.Visit:
-            return (joinedload(models.Visit.patient),
-                    joinedload(models.Visit.doctor),
-                    joinedload(models.Visit.diagnose),
-                    joinedload(models.Visit.purpose))
-        case models.Doctor:
-            return (selectinload(models.Doctor.visits),
-                    joinedload(models.Doctor.section))
-        case models.Patient:
-            return (selectinload(models.Patient.visits),
-                    joinedload(models.Patient.section))
-        case models.Section:
-            return (selectinload(models.Section.doctors),
-                    selectinload(models.Section.patients))
-        case models.Diagnose:
-            return (selectinload(models.Diagnose.visits), )
-        case models.Purpose:
-            return (selectinload(models.Purpose.visits), )
-        case _:
-            return ()
+        query = (select(table.ORM).options(*table.OPTIONS).filter_by(**{column: value})
+                 if relationships else
+                 select(table.ORM).filter_by(**{column: value}))
+        return [dto.model_validate(row, from_attributes=True)
+                for row in (await session.execute(query)).unique().scalars().all()]
