5e29fdb6aac1976020bf83e7d99d818aac399938
FranChesKo <khl_doss@mail.ru>
2023-12-17 13:35:26 +0800

    Add tables shcemas and models types

---
+++ src/database/tables/__init__.py
@@ -0,0 +1,12 @@
+from .tables import TableType, BaseTable, Visit, Doctor, Patient, Section, Diagnose, Purpose
+
+__all__ = [
+    "TableType",
+    "BaseTable",
+    "Visit",
+    "Doctor",
+    "Patient",
+    "Section",
+    "Diagnose",
+    "Purpose"
+]
